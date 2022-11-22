b346557d5918e7103e6e21789c1e2510e6c1fb31
Artem <xan232@gmail.com>
2022-11-22 17:17:40 +0300

    added reader

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -5,8 +7,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    for i in range(4):
-        a = input()
-        print_hi(a)
+    for x in sys.stdin.readlines():
+        print_hi(x.rstrip())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
